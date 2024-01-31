0a84b3a6abf888214b701e5c11b7065070634ed1
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-01-31 19:46:28 +0600

    feat: added FetchableState type

--- src/types/store.types.ts
+++ src/types/store.types.ts
@@ -1,5 +1,6 @@
-import type { Action, ThunkAction } from '@reduxjs/toolkit'
+import type { Action, SerializedError, ThunkAction } from '@reduxjs/toolkit'
 
+import type { FetchState } from '@/core'
 import type { store } from '@/features'
 
 export type AppStoreDispatch = typeof store.dispatch
@@ -12,3 +13,9 @@ export type AppStoreThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >
+
+export interface FetchableState {
+  state: FetchState
+  error: SerializedError | null
+  requestId: string | null
+}
