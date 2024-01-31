de758961696a0e808c75257b21ed5bec1dca4dd3
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-01-31 16:51:14 +0600

    feat: added shadcn Separator component

---
+++ src/components/ui/separator.tsx
@@ -0,0 +1,29 @@
+import { Root } from '@radix-ui/react-separator'
+import type { ComponentPropsWithoutRef, ElementRef } from 'react'
+import { forwardRef } from 'react'
+
+import { cn } from '@/api'
+
+type Component = typeof Root
+type Ref = ElementRef<Component>
+type RefProps = ComponentPropsWithoutRef<Component>
+
+export type SeparatorProps = RefProps
+
+export const Separator = forwardRef<Ref, SeparatorProps>(function Separator(props, ref) {
+  const { orientation = 'horizontal', decorative = true, className, ...restProps } = props
+
+  return (
+    <Root
+      ref={ref}
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        'shrink-0 bg-border',
+        orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
+        className,
+      )}
+      {...restProps}
+    />
+  )
+})
