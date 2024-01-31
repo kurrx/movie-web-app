dd831d05b539fc9f1f5e2b7bc726f754099b4e6f
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-01 00:57:40 +0600

    feat: added useStoreBoolean hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useAppViewport'
 export * from './useElementRect'
 export * from './useStore'
+export * from './useStoreBoolean'
---
+++ src/hooks/useStoreBoolean.ts
@@ -0,0 +1,22 @@
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit'
+import { SetStateAction, useCallback } from 'react'
+
+import { useAppDispatch } from './useStore'
+
+export type UseStoreBooleanAction = ActionCreatorWithPayload<SetStateAction<boolean>, string>
+
+export function useBooleanStore(action: UseStoreBooleanAction) {
+  const dispatch = useAppDispatch()
+
+  const set = useCallback(
+    (value: SetStateAction<boolean>) => {
+      dispatch(action(value))
+    },
+    [dispatch, action],
+  )
+  const setTrue = useCallback(() => set(true), [set])
+  const setFalse = useCallback(() => set(false), [set])
+  const toggle = useCallback(() => set((prev) => !prev), [set])
+
+  return { set, setTrue, setFalse, toggle }
+}
