fe71af4907154728047c25e94f88eb539d850908
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-01-31 18:21:03 +0600

    feat: added ErrorView component

---
+++ src/views/ErrorView.tsx
@@ -0,0 +1,35 @@
+import { cn } from '@/api'
+import { Separator } from '@/components'
+import { Title } from '@/features'
+
+const classes = {
+  root: cn('flex-1 flex flex-col items-center justify-center'),
+  content: cn(
+    'flex flex-col items-center justify-center sm:space-x-4',
+    'sm:space-y-0 space-y-0 space-y-4 sm:flex-row',
+  ),
+  title: cn('font-bold text-3xl'),
+  subtitle: cn('text-muted-foreground max-w-[280px] text-center sm:text-left'),
+}
+
+export interface ErrorViewProps {
+  title?: string
+  subtitle?: string
+  docTitle?: string
+}
+
+export function ErrorView(props: ErrorViewProps) {
+  const { title, subtitle, docTitle } = props
+
+  return (
+    <div className={classes.root}>
+      <Title>{docTitle || 'Not Found'}</Title>
+      <div className={classes.content}>
+        <h1 className={classes.title}>{title || '404'}</h1>
+        <Separator orientation='vertical' className='hidden sm:block' />
+        <Separator orientation='horizontal' className='sm:hidden block' />
+        <p className={classes.subtitle}>{subtitle || 'Page not found.'}</p>
+      </div>
+    </div>
+  )
+}
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,2 +1,3 @@
+export * from './ErrorView'
 export * from './HomeView'
 export * from './RedirectView'
