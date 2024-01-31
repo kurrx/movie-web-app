9d231f28d1795f5f2482a0ab21721f57b17006c2
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-01-31 18:29:08 +0600

    style: added loader icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite-plugin-svgr/client" />
 export { default as GithubLogoIcon } from './github-logo.svg?react'
+export { default as LoaderIcon } from './loader.svg?react'
 export { default as LogoIcon } from './logo.svg?react'
 export { default as MenuIcon } from './menu.svg?react'
 export { default as XLogoIcon } from './x-logo.svg?react'
