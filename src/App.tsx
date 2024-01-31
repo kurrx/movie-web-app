464792aa70d113ff04ac0ed2047dfc9af68e74e2
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-01-31 13:39:32 +0600

    feat: added shadcn Tooltip component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import '@/App.css'
 import { Provider as StoreProvider } from 'react-redux'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import { TooltipProvider } from '@/components'
 import { FeaturesProvider, store } from '@/features'
 import { useAppViewport } from '@/hooks'
 
@@ -12,13 +13,15 @@ export function App() {
   return (
     <StoreProvider store={store}>
       <FeaturesProvider>
-        <BrowserRouter>
-          <main id='content'>
-            <Routes>
-              <Route path='*' element={null} />
-            </Routes>
-          </main>
-        </BrowserRouter>
+        <TooltipProvider delayDuration={200}>
+          <BrowserRouter>
+            <main id='content'>
+              <Routes>
+                <Route path='*' element={null} />
+              </Routes>
+            </main>
+          </BrowserRouter>
+        </TooltipProvider>
       </FeaturesProvider>
     </StoreProvider>
   )
---
+++ src/components/ui/tooltip.tsx
@@ -0,0 +1,47 @@
+import { Content, Portal, Provider, Root, Trigger } from '@radix-ui/react-tooltip'
+import type { ComponentPropsWithoutRef, ElementRef } from 'react'
+import { forwardRef } from 'react'
+
+import { cn } from '@/api'
+
+export const TooltipProvider = Provider
+export const Tooltip = Root
+export const TooltipTrigger = Trigger
+
+type Component = typeof Content
+type Ref = ElementRef<Component>
+type RefProps = ComponentPropsWithoutRef<Component>
+
+export interface TooltipContentProps extends RefProps {
+  container?: HTMLElement | null
+}
+
+export const TooltipContent = forwardRef<Ref, TooltipContentProps>(
+  function TooltipContent(props, ref) {
+    const { className, container, sideOffset = 4, ...restProps } = props
+
+    return (
+      <Portal container={container}>
+        <Content
+          ref={ref}
+          sideOffset={sideOffset}
+          className={cn(
+            'z-50 overflow-hidden rounded-md bg-primary',
+            'px-3 py-1.5 text-xs text-primary-foreground',
+            'animate-in fade-in-0 zoom-in-95',
+            'data-[state=closed]:animate-out',
+            'data-[state=closed]:fade-out-0',
+            'data-[state=closed]:zoom-out-95',
+            'data-[side=bottom]:slide-in-from-top-2',
+            'data-[side=left]:slide-in-from-right-2',
+            'data-[side=right]:slide-in-from-left-2',
+            'data-[side=top]:slide-in-from-bottom-2',
+            'select-none',
+            className,
+          )}
+          {...restProps}
+        />
+      </Portal>
+    )
+  },
+)
