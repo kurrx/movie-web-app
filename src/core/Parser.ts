b0e8bf3e26ef6f37de23bb0ecece5e13d1bb29f5
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-01-31 19:54:28 +0600

    feat: added html Parser utility Class

---
+++ src/core/Parser.ts
@@ -0,0 +1,86 @@
+import { trimStr } from '@/api'
+import { Conditional } from '@/types'
+
+export class Parser {
+  private parent: ParentNode
+  private defaultError: string
+
+  constructor(parent: ParentNode) {
+    this.parent = parent
+    this.defaultError = 'Element is not available.'
+  }
+
+  public setParent(parent: ParentNode) {
+    this.parent = parent
+  }
+
+  public setDefaultError(error: string) {
+    this.defaultError = error
+  }
+
+  public select<B extends boolean>(
+    selector: string,
+    required: B = false as B,
+  ): Conditional<B, Element> {
+    const element = this.parent.querySelector(selector)
+    if (!element && required) {
+      throw new Error(this.defaultError)
+    }
+    return element as Element
+  }
+
+  public contains(className: string) {
+    if (this.parent instanceof Element) {
+      return this.parent.classList.contains(className)
+    }
+    return false
+  }
+
+  public hasChild(selector: string) {
+    return this.parent.querySelector(selector) !== null
+  }
+
+  public switchToChild<B extends boolean>(selector: string, required: B = false as B) {
+    const newParent = this.select<B>(selector, required)
+    if (newParent) {
+      this.setParent(newParent)
+    }
+    return newParent
+  }
+
+  public all(selector: string) {
+    return Array.from(this.parent.querySelectorAll(selector))
+  }
+
+  public attr(name: string) {
+    if (!(this.parent instanceof Element)) {
+      return null
+    }
+    const value = this.parent.getAttribute(name)
+    return value ? trimStr(value) : value
+  }
+
+  public attrInt(name: string) {
+    const value = this.attr(name)
+    if (value) {
+      const int = parseInt(value)
+      if (!isNaN(int)) {
+        return int
+      }
+    }
+    return null
+  }
+
+  public attrEnum<T extends string>(name: string, values: T[]) {
+    const value = this.attr(name)
+    if (value && values.includes(value as T)) {
+      return value as T
+    }
+    return null
+  }
+
+  public text() {
+    const text = this.parent.textContent
+    return text ? trimStr(text) : null
+  }
+}
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,4 @@
 // eslint-disable-next-line simple-import-sort/exports
+export * from './Parser'
 export * from './Request'
 export * from './enums'
