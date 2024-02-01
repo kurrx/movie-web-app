1befef2f68ed45276b78cafa4830fb281242d6c7
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-01 15:45:59 +0600

    feat: added Thumbnails parser utility class

---
+++ src/core/Thumbnails.ts
@@ -0,0 +1,87 @@
+export interface ThumbnailSegment {
+  start: number
+  end: number
+  url: string
+  width: number
+  height: number
+  x: number
+  y: number
+}
+
+export class Thumbnails {
+  segments: ThumbnailSegment[]
+  segmentDuration: number
+
+  constructor() {
+    this.segments = []
+    this.segmentDuration = 1
+  }
+
+  parse(contents: string) {
+    contents = contents.replaceAll('WEBVTT\n\n', '')
+    const lines = contents.split('\n\n')
+    for (const line of lines) {
+      const [time, url] = line.split('\n')
+      const [start, end] = time.split(' --> ').map((t) => {
+        const [hms, ms] = t.split('.')
+        const [h, m, s] = hms.split(':').map((n) => parseInt(n))
+        return h * 3600 + m * 60 + s + parseInt(ms) / 1000
+      })
+      const [cleanUrl, xywh] = url.split('#xywh=')
+      const [x, y, width, height] = xywh.split(',').map((n) => parseInt(n))
+      this.segments.push({
+        start,
+        end,
+        url: cleanUrl,
+        width,
+        height,
+        x,
+        y,
+      })
+    }
+    this.segmentDuration = this.segments[0].end - this.segments[0].start
+  }
+
+  getSegment(time: number) {
+    const segmentIndex = Math.floor(time / this.segmentDuration)
+    if (segmentIndex >= this.segments.length) return undefined
+    return this.segments[segmentIndex]
+  }
+
+  getPreviewSegment(time: number, preferedWidth: number) {
+    const segment = this.getSegment(time)
+    if (!segment) return undefined
+    const ratio = preferedWidth / segment.width
+    const width = segment.width * ratio
+    const height = segment.height * ratio
+    return {
+      width,
+      height,
+      backgroundPosition: `${-segment.x * ratio}px ${-segment.y * ratio}px`,
+      backgroundSize: `${width * 5}px ${height * 5}px`,
+      backgroundImage: `url(${segment.url})`,
+    }
+  }
+
+  getOverlaySegment(time: number, containerWidth: number, containerHeight: number) {
+    const segment = this.getSegment(time)
+    if (!segment) return undefined
+    const videoRatio = segment.width / segment.height
+    const containerRatio = containerWidth / containerHeight
+    let ratio = 1
+    if (videoRatio < containerRatio) {
+      ratio = containerHeight / segment.height
+    } else {
+      ratio = containerWidth / segment.width
+    }
+    const width = segment.width * ratio
+    const height = segment.height * ratio
+    return {
+      width,
+      height,
+      backgroundPosition: `${-segment.x * ratio}px ${-segment.y * ratio}px`,
+      backgroundSize: `${width * 5}px ${height * 5}px`,
+      backgroundImage: `url(${segment.url})`,
+    }
+  }
+}
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,5 @@
 // eslint-disable-next-line simple-import-sort/exports
 export * from './Parser'
 export * from './Request'
+export * from './Thumbnails'
 export * from './enums'
