aa351305f1d799881752ee81a9a7e0cf8cb1bf2f
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-01 22:50:44 +0600

    feat: added download icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite-plugin-svgr/client" />
+export { default as DownloadIcon } from './download.svg?react'
 export { default as GithubLogoIcon } from './github-logo.svg?react'
 export { default as LoaderIcon } from './loader.svg?react'
 export { default as LogoIcon } from './logo.svg?react'
