6133c486abf49b49d1f0eff3c5edc6424acc9e37
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-01 15:45:43 +0600

    feat: added utility functions

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -38,3 +38,48 @@ export function stringToBase64(str: string) {
 export function base64ToString(base64: string) {
   return Buffer.from(base64, 'base64').toString('utf8')
 }
+
+export function unite(arr: string[][]) {
+  const final: string[] = []
+  for (const row of arr) {
+    final.push(row.join(''))
+  }
+  return final
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function product(...args: any[]) {
+  let argv = Array.prototype.slice.call(args)
+  const argc = argv.length
+  if (argc === 2 && !isNaN(argv[argc - 1])) {
+    const copies = []
+    for (let i = 0; i < argv[argc - 1]; i++) {
+      copies.push(argv[0].slice()) // Clone
+    }
+    argv = copies
+  }
+  return argv.reduce(
+    function tl(accumulator, value) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const tmp: any = []
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      accumulator.forEach(function (a0: any) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        value.forEach(function (a1: any) {
+          tmp.push(a0.concat(a1))
+        })
+      })
+      return tmp
+    },
+    [[]],
+  )
+}
+
+export function bytesToStr(bytes: number, digits = 2) {
+  if (bytes === 0) return '0 Bytes'
+  const k = 1024
+  const dm = digits < 0 ? 0 : digits
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB']
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + sizes[i]
+}
