58c0b177b5bda1ec353db3a6a5cc3e994577222b
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-03 16:24:22 +0600

    feat: added Video context value to nodes context

--- src/features/player/components/PlayerNodes.tsx
+++ src/features/player/components/PlayerNodes.tsx
@@ -7,6 +7,7 @@ import { useContextWrapper } from '../hooks'
 type Container = HTMLDivElement | null
 type Content = HTMLDivElement | null
 type Player = ReactPlayer | null
+type Video = HTMLVideoElement | null
 
 export interface PlayerNodes {
   container: Container
@@ -17,6 +18,8 @@ export interface PlayerNodes {
 
   player: Player
   setPlayer: Dispatch<SetStateAction<Player>>
+
+  video: Video
 }
 
 const PlayerNodesContext = createContext<PlayerNodes | null>(null)
@@ -27,9 +30,10 @@ export function PlayerNodesProvider({ children }: PropsWithChildren) {
   const [container, setContainer] = useState<Container>(null)
   const [content, setContent] = useState<Content>(null)
   const [player, setPlayer] = useState<Player>(null)
+  const video = useMemo(() => (player?.getInternalPlayer() || null) as Video, [player])
   const context = useMemo<PlayerNodes>(
-    () => ({ container, setContainer, content, setContent, player, setPlayer }),
-    [container, content, player],
+    () => ({ container, setContainer, content, setContent, player, setPlayer, video }),
+    [container, content, player, video],
   )
 
   return <PlayerNodesContext.Provider value={context}>{children}</PlayerNodesContext.Provider>
