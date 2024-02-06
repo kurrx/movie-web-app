6e207389fde2acc703d87b687859f365e1dcf125
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-06 22:54:22 +0600

    fix: extra gap on top of navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import '@/App.css'
 import { Provider as StoreProvider } from 'react-redux'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { Footer, Navbar, TooltipProvider } from '@/components'
+import { Footer, Navbar, ScrollArea, TooltipProvider } from '@/components'
 import { FeaturesProvider, SearchDialog, store } from '@/features'
 import { useAppViewport } from '@/hooks'
 import { VPNMiddleware } from '@/middlewares'
@@ -18,17 +18,19 @@ export function App() {
         <BrowserRouter>
           <FeaturesProvider>
             <Navbar />
-            <main id='content'>
-              <VPNMiddleware>
-                <SearchDialog />
-                <Routes>
-                  <Route path='/' element={<HomeView />} />
-                  <Route path='/watch/:typeId/:genreId/:slug' element={<WatchView />} />
-                  <Route path='*' element={<RedirectView to='/' />} />
-                </Routes>
-              </VPNMiddleware>
-            </main>
-            <Footer />
+            <ScrollArea>
+              <main id='content'>
+                <VPNMiddleware>
+                  <SearchDialog />
+                  <Routes>
+                    <Route path='/' element={<HomeView />} />
+                    <Route path='/watch/:typeId/:genreId/:slug' element={<WatchView />} />
+                    <Route path='*' element={<RedirectView to='/' />} />
+                  </Routes>
+                </VPNMiddleware>
+              </main>
+              <Footer />
+            </ScrollArea>
           </FeaturesProvider>
         </BrowserRouter>
       </TooltipProvider>
