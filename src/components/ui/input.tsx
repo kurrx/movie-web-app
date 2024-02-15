e5dc64a437a4fb18d41feae10b8b539149326bbd
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-15 10:55:14 +0600

    feat: added Input component

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,33 @@
+import { forwardRef, InputHTMLAttributes } from 'react'
+
+import { cn } from '@/api'
+
+type Ref = HTMLInputElement
+type RefProps = InputHTMLAttributes<Ref>
+
+export type InputProps = RefProps
+
+export const Input = forwardRef<Ref, InputProps>(function Input(props, ref) {
+  const { className, type, ...restProps } = props
+
+  return (
+    <input
+      ref={ref}
+      type={type}
+      className={cn(
+        'flex h-9 w-full rounded-md border border-input',
+        'bg-transparent px-3 py-1 text-sm shadow-sm',
+        'transition-colors file:border-0 file:bg-transparent',
+        'file:text-sm file:font-medium',
+        'placeholder:text-muted-foreground',
+        'focus-visible:outline-none',
+        'focus-visible:ring-1',
+        'focus-visible:ring-ring',
+        'disabled:cursor-not-allowed',
+        'disabled:opacity-50',
+        className,
+      )}
+      {...restProps}
+    />
+  )
+})
