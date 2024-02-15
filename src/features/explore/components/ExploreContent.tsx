863d04c8ae40ca63db665e03843b86f3baf48068
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-16 01:11:10 +0600

    refactor: moved explore url validation to one component level above

--- src/features/explore/components/ExploreContent.tsx
+++ src/features/explore/components/ExploreContent.tsx
@@ -1,3 +1,34 @@
+import { useCallback, useEffect, useMemo } from 'react'
+
+import { FetchState } from '@/core'
+import { useStore } from '@/hooks'
+import { FallbackView } from '@/views'
+
+import { exploreSearch, selectExploreResult } from '../explore.slice'
+
 export function ExploreContent({ url }: { url: string }) {
-  return null
+  const [dispatch, selector] = useStore()
+  const exploreItem = selector((state) => selectExploreResult(state, url))
+  const state = useMemo(() => exploreItem?.state || FetchState.LOADING, [exploreItem])
+
+  const get = useCallback(() => {
+    const signal = dispatch(exploreSearch(url))
+    return () => {
+      signal.abort()
+    }
+  }, [dispatch, url])
+
+  useEffect(get, [get])
+
+  return (
+    <FallbackView
+      dismissible
+      state={state}
+      text='Loading titles...'
+      error={exploreItem?.error}
+      onReload={get}
+    >
+      <div className='w-full max-w-full flex-1 flex flex-col'></div>
+    </FallbackView>
+  )
 }
