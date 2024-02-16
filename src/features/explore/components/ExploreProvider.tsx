2adfdbbd5fd2afa6dd10c85f34e0b54c8739b09b
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-17 00:07:24 +0600

    chore: type naming

--- src/features/explore/components/ExploreProvider.tsx
+++ src/features/explore/components/ExploreProvider.tsx
@@ -3,12 +3,12 @@ import { ExploreResponse } from '@/types'
 
 import { selectExploreResponse } from '../explore.slice'
 
-export interface ExploreContentProviderProps {
+export interface ExploreProviderProps {
   url: string
   children: (response: ExploreResponse) => React.ReactNode
 }
 
-export function ExploreProvider({ url, children }: ExploreContentProviderProps) {
+export function ExploreProvider({ url, children }: ExploreProviderProps) {
   const response = useAppSelector((state) => selectExploreResponse(state, url))
 
   return <div className='w-full max-w-full flex-1 flex flex-col'>{children(response)}</div>
