92e279dbb2972e296c620e596830d540fe6b5d60
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-21 17:47:06 +0600

    fix: scroll to top only if pathname and search queries is changed

--- src/features/router/hooks/useScrollTop.ts
+++ src/features/router/hooks/useScrollTop.ts
@@ -1,12 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export function useScrollTop() {
-  const location = useLocation()
+  const { pathname, search } = useLocation()
+  const url = useMemo(() => pathname + search, [pathname, search])
 
   useEffect(() => {
     const scrollElement = document.querySelector('#app-scroll > [data-radix-scroll-area-viewport]')
     if (!scrollElement) return
     scrollElement.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-  }, [location])
+  }, [url])
 }
