a7b65a67aa2c28fd5c518c9b4309e25d5e5ac83a
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-22 14:45:04 +0600

    style: fixed collection card loader

--- src/features/explore/components/Collections/ExploreCollectionCardLoader.tsx
+++ src/features/explore/components/Collections/ExploreCollectionCardLoader.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components'
 export function ExploreCollectionCardLoader() {
   return (
     <div className='w-full'>
-      <div className='relative w-full rounded-xl overflow-hidden light'>
+      <div className='relative w-full rounded-xl overflow-hidden'>
         <div className='pb-[calc((15/26)*100%)]' />
         <Skeleton className='absolute w-full h-full top-0 left-0' />
       </div>
