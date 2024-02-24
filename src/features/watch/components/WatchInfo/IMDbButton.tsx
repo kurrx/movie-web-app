e3f2ad6a48aff3a3d91c141805ceb304efc3cd14
Ruslan Kurbanali <kurr.eax@gmail.com>
2024-02-24 12:57:54 +0600

    fix: imdb rating to fixed

--- src/features/watch/components/WatchInfo/IMDbButton.tsx
+++ src/features/watch/components/WatchInfo/IMDbButton.tsx
@@ -13,7 +13,7 @@ export function IMDbButton({ rating }: { rating: ItemRating }) {
     >
       <a href={rating.link} target='_blank' rel='noreferrer'>
         <IMDbLogoIcon className='mr-2 h-4 w-8' />
-        {rating.rate}
+        {rating.rate.toFixed(1)}
         <span className='opacity-50 font-normal text-[0.6rem] leading-[0.6rem] ml-1'>
           ({formatter.format(rating.votes)})
         </span>
